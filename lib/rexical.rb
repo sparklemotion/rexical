7c5e59cf4b2258779c1db044c341a4f152bf299d
Aaron Patterson <aaron.patterson@gmail.com>
2010-06-15 14:22:02 -0700

    updating to beta gem

--- lib/rexical.rb
+++ lib/rexical.rb
@@ -2,7 +2,7 @@ require 'rexical/generator'
 require 'rexical/rexcmd'
 
 module Rexical
-  VERSION = "1.0.4"
+  VERSION = "1.0.5.beta1"
   Copyright = 'Copyright (c) 2005-2006 ARIMA Yasuhiro'
   Mailto    = 'arima.yasuhiro@nifty.com'
 end
