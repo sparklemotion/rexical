918571ceb1680369229687dfb724100184383470
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-08 15:48:03 -0800

    terrorist version bump

--- lib/rexical.rb
+++ lib/rexical.rb
@@ -2,7 +2,7 @@ require 'rexical/generator'
 require 'rexical/rexcmd'
 
 module Rexical
-  VERSION = "1.0.5.beta2"
+  VERSION = "1.0.5"
   Copyright = 'Copyright (c) 2005-2006 ARIMA Yasuhiro'
   Mailto    = 'arima.yasuhiro@nifty.com'
 end
