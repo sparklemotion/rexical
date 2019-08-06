2c1cd18505cf99c1540f5e65f57bc9e2e2396f32
Mike Dalessio <mike.dalessio@gmail.com>
2019-08-06 13:20:30 -0400

    version bump to v1.0.7

--- lib/rexical.rb
+++ lib/rexical.rb
@@ -1,8 +1,8 @@
-require 'rexical/generator'
-require 'rexical/rexcmd'
+require "rexical/generator"
+require "rexical/rexcmd"
 
 module Rexical
-  VERSION = "1.0.5"
-  Copyright = 'Copyright (c) 2005-2006 ARIMA Yasuhiro'
-  Mailto    = 'arima.yasuhiro@nifty.com'
+  VERSION = "1.0.7"
+  Copyright = "Copyright (c) 2005-2006 ARIMA Yasuhiro"
+  Mailto = "arima.yasuhiro@nifty.com"
 end
